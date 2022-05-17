8f46b89648a1df372a4a4500b07fcd6285d36fce
Nicolás Díaz <nikodiaz56@gmail.com>
2022-05-17 19:04:31 -0300

    version 1.0

--- src/main.js
+++ src/main.js
@@ -1,2 +1,150 @@
-import { API_KEY } from "./private.js";
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  },
+  params: {
+    'api_key': API_KEY,
+  }
+});
 
+/////////////////////////////////////////////////////////////
+//////////////////////////UTILS//////////////////////////////
+/////////////////////////////////////////////////////////////
+
+function createMovies(movies, container) {
+  container.innerHTML = '';
+
+
+  movies.forEach(movie => {
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+    movieContainer.addEventListener('click', () => {
+      location.hash = `#movie=${movie.id}`;
+    })
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.src = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
+    movieImg.alt = movie.title;
+    const movieTitle = document.createElement('p');
+    movieTitle.classList.add('movie-title');
+    movieTitle.textContent = movie.title;
+    
+    movieContainer.appendChild(movieImg);
+    movieContainer.appendChild(movieTitle);
+    container.appendChild(movieContainer);
+  });
+}
+
+function createCategories(categories, container) {
+  container.innerHTML = '';
+
+  categories.forEach(category => {
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.id = `id${category.id}`;
+    categoryTitle.addEventListener('click', () => {
+      location.hash = `#category=${category.id}-${category.name}`;
+    })
+    categoryTitle.textContent = category.name;
+    
+    categoryContainer.appendChild(categoryTitle);
+    container.appendChild(categoryContainer);
+  });
+}
+
+/////////////////////////////////////////////////////////////
+///////////////////////TRENDING//////////////////////////////
+/////////////////////////////////////////////////////////////
+
+async function getTrending() {
+  const { data } = await api('trending/movie/day');
+
+  const movies = data.results;
+
+  createMovies(movies, trendingList);
+
+}
+
+async function getTrendingMovies() {
+  const { data } = await api('trending/movie/day');
+
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+
+}
+
+/////////////////////////////////////////////////////////////
+///////////////////////////CATEGORIES////////////////////////
+/////////////////////////////////////////////////////////////
+
+async function getCategories() {
+  const { data } = await api('genre/movie/list?language=en-US');
+
+  const categories = data.genres;
+
+  createCategories(categories, categoriesList);
+}
+
+/////////////////////////////////////////////////////////////
+///////////////////FILTER BY CATEGORIES//////////////////////
+/////////////////////////////////////////////////////////////
+
+async function getMoviesByCategory(id) {
+  const { data } = await api('discover/movie', {
+    params: { 
+      with_genres: id,
+     }
+  });
+
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+
+}
+
+/////////////////////////////////////////////////////////////
+///////////////////////SEARCH MOVIES/////////////////////////
+/////////////////////////////////////////////////////////////
+
+async function getMoviesBySearch(query) {
+  const { data } = await api('search/movie', {
+    params: { 
+      query: query,
+     }
+  });
+
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+
+}
+
+/////////////////////////////////////////////////////////////
+/////////////////////MOVIE DETAILS///////////////////////////
+/////////////////////////////////////////////////////////////
+
+async function getMovieById(id) {
+  const { data: movie } = await api('movie/'+ id);
+
+  movieDetailImg.src = `https://image.tmdb.org/t/p/w300${movie.poster_path}`;
+  movieDetailTitle.textContent = movie.title;
+  movieDetailDescription.textContent = movie.overview;
+  movieDetailScore.textContent = movie.vote_average;
+
+  createCategories(movie.genres, movieDetailCategoriesList);
+  getRelatedMoviesById(id);
+}
+
+async function getRelatedMoviesById(id) {
+  const { data } = await api(`movie/${id}/recommendations`);
+  const relatedMovies = data.results;
+  console.log(data);
+
+  createMovies(relatedMovies, relatedMoviesContainer);
+
+}
---
+++ src/nodes.js
@@ -0,0 +1,35 @@
+// Sections
+
+const headerSection = document.querySelector('#header');
+const categoriesSection = document.querySelector('#categories');
+const trendingSection = document.querySelector('#trending');
+const genericSection = document.querySelector('#genericList');
+const movieDetailSection = document.querySelector('#movieDetail');
+
+// List & Containers
+
+const searchForm = document.querySelector('#searchForm');
+const trendingList = document.querySelector('.trending-movieList');
+const categoriesList = document.querySelector('.categories-list');
+const movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list');
+const relatedMoviesContainer = document.querySelector('.relatedMovies-scrollContainer');
+const movieDetailContainer = document.querySelector('#movieDetail .movie-container');
+
+// Elements 
+
+const headerTitle = document.querySelector('.header-title');
+const arrowBtn = document.querySelector('.header-arrow');
+const headerCategoryTitle = document.querySelector('.header-title--category');
+
+const trendingBtn = document.querySelector('#trending .viewMore-btn');
+
+const categoriesHeader = document.querySelector('.categories-header');
+const categoriesBtn = document.querySelector('#categories .viewMore-btn');
+
+const searchFormInput = document.querySelector('#searchForm input');
+const searchFormBtn = document.querySelector('#searchForm button');
+
+const movieDetailImg = document.querySelector('#movieDetail .movie-img');
+const movieDetailTitle = document.querySelector('.movieDetail-title');
+const movieDetailDescription = document.querySelector('.movieDetail-description');
+const movieDetailScore = document.querySelector('.movieDetail-score');
